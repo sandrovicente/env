66b2df26ecaa17c1f199a49aa7104b496bd710d3
Sandro AV <sandro.vicente@smarkets.com>
2018-06-29 10:25:01 +0100

    environment changes

--- ubuntu-sm/.emacs.d/my/my.el
+++ ubuntu-sm/.emacs.d/my/my.el
@@ -1,18 +1,12 @@
 "
-*** installed   
+package-list-packages
 
-X back-button        0.6.6         installed             Visual navigation through mark rings
   helm               2.9.6         installed             Helm is an Emacs incremental and narrowing framework
+  helm-projectile    0.14.0        installed             Helm integration for Projectile
   ido-better-flex    0.2           installed             A better flex (fuzzy) algorithm for Ido.
   magit              20171214.432  installed             A Git porcelain inside Emacs.
+  projectile         0.14.0        installed             Manage and navigate projects in Emacs easily
   use-package        20171210.2136 installed             A configuration macro for simplifying your .emacs
-  projectile
-  helm-projectile
-"
-
-"
-(require 'back-button)
- (back-button-mode 1)
 "
 
 (add-to-list 'package-archives
@@ -20,7 +14,16 @@ X back-button        0.6.6         installed             Visual navigation throu
 
 (defun git-grep (regexp)
   (interactive "sSearch in git repo: ")
-  (grep (format "GIT_PAGER='' git grep -nH --no-color -i \"%s\" -- $(git rev-parse --show-toplevel)" regexp)))
+  (grep-find (format "GIT_PAGER='' git grep -nH --no-color \"%s\" -- $(git rev-parse --show-toplevel)" regexp)))
+
+(defun git-grep-w (regexp &optional word ext)
+  (interactive "sSearch in git repo:
+sWord
+sExt")
+  (let ((grep-str (format "GIT_PAGER='' git grep -nH \"%s\" " regexp)))
+    (message ">>> %s" grep-str)
+    (grep-find (grep-str))))
+  
 
 (global-set-key (kbd "C-x ?") 'git-grep)
 
@@ -32,6 +35,8 @@ X back-button        0.6.6         installed             Visual navigation throu
 (global-set-key (kbd "C-s-f") 'helm-find-files)
 (global-set-key (kbd "C-s-r") 'helm-recentf)
 (global-set-key (kbd "C-s-b") 'helm-buffers-list)
+
+(global-set-key (kbd "C-M-g") 'helm-grep-do-git-grep)
 
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
