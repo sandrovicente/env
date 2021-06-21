b8b1db6e83d8516b3af2aa5e9b2879caf2c0461a
sandrovicente <sandrovicente@gmail.com>
2021-06-21 21:41:03 +0100

    update w/ coc nvim

--- mac/init.sh
+++ mac/init.sh
@@ -6,13 +6,13 @@ alias vim=/usr/local/bin/vim
 
 alias kdiff3=/Applications/kdiff3.app/Contents/MacOS/kdiff3
 
-function xemacs() { /Users/sandrovicente/workspace/prj/emacs/nextstep/Emacs.app/Contents/MacOS/Emacs  "$@" >/dev/null 2>&1 & }
+function xemacs() { /Users/sandrovicente/workspace/prjs/emacs/nextstep/Emacs.app/Contents/MacOS/Emacs  "$@" >/dev/null 2>&1 & }
 function code() { /Applications/Visual\ Studio\ Code.app/Contents/MacOS/Electron "$@" >/dev/null 2>&1 & }
 
 #alias gdb=lldb
 
 export JAVA_HOME=`/usr/libexec/java_home -v 13.0.2`
-export BOOST_HOME=/Users/sandrovicente/workspace/prj/boost_1_73_0
+export BOOST_HOME=/Users/sandrovicente/workspace/prjs/boost_1_73_0
 
 export MAVEN_HOME=/Users/sandrovicente/workspace/tools/apache-maven-3.6.3
 export PATH=$PATH:$MAVEN_HOME/bin
@@ -42,3 +42,7 @@ export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 alias nvim=$W/tools/nvim-osx64/bin/nvim
 
 export PATH=$PATH:$HOME/Library/Python/3.7/bin
+
+gnvim() {
+    $W/tools/VimR.app/Contents/Resources/vimr "$@" >/dev/null 2>&1 
+}
